b8956298c247de0cc9d354b183e6e36b1e2cdbc2
byron <byron@symphony.com>
2015-12-02 18:16:10 -0800

    git cmd

---
+++ gitCmd/createAndPushOriginBranchFromUpstream.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+if [ "help"="$1" ] || [ "h"="$1" ] || [ "-h"="$1" ]; then
+    echo "new_branch_name upstream_branch_name"
+    exit 1
+fi
+
+
+if [ -z "$1" ]; then
+    echo "new branch name is empty"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "upstream branch name is empty"
+    exit 1
+fi
+
+me
+
+echo "create branch $1 base on upstream $2"
+
+echo "git checkout -t -b $1 upstream/$2"
+git checkout -t -b $1 upstream/$2
+echo "======================"
+
+echo "git push origin $1"
+git push origin $1
+echo "======================"
+
+echo "git checkout dev"
+git checkout dev
+echo "======================"
+
+echo "git branch -d $1"
+git branch -d $1
+echo "======================"
+
+echo "git checkout $1"
+git checkout $1
+echo "======================"
+
+echo "git status"
+git status
+echo "======================"
+
+echo "prefect done!"
